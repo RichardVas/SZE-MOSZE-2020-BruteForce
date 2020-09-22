aac0511c42103a4538eeeb3ca78dd065ae159962
Richard Vas <58149185+RichardVas@users.noreply.github.com>
2020-09-22 18:59:47 +0200

    Implement the methods declared in Fighter.h

---
+++ Fighter.cpp
@@ -0,0 +1,22 @@
+#include "Fighter.h"
+
+
+/*combat mechanics*/
+
+void Fighter :: take_dmg(Fighter& enemy) {
+	HP -= enemy.getDMG();
+	if (HP < 0)
+		HP = 0;
+}
+
+void Fighter :: deal_dmg(Fighter &enemy) { 
+	enemy.take_dmg(*this);
+}
+
+std::ostream& operator<<(std::ostream& os, const Fighter& fi)
+{
+	os << fi.getName() << ": HP: " << fi.getHP() << "," << " DMG: " << fi.getDMG() << std::endl;
+	return os;
+}
+
+
