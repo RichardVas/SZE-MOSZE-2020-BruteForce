31c5c28336d46e951068cde38f1d38d01093e191
Richard Vas <58149185+RichardVas@users.noreply.github.com>
2020-09-24 21:37:21 +0200

    Add parseUnit method
    Extended the fighter class with parseUnit, to process json files.

--- Fighter.cpp
+++ Fighter.cpp
@@ -20,3 +20,53 @@ std::ostream& operator<<(std::ostream& os, const Fighter& fi)
 }
 
 
+Fighter Fighter::parseUnit(std::string fname)
+{
+	std::ifstream file;
+
+
+	std::string name = "";
+	int hp = -1;
+	int dmg = -1;
+
+
+	file.open(fname);
+	if (!file.good()) throw std::runtime_error("File cannot be opened!");
+	
+	else {
+		int i = 0;
+
+		std::string line;
+		while (std::getline(file, line))
+		{
+
+			std::string str1 = "";
+			std::string str2 = "";
+			if (line[0] != '{' && line[0] != '}')
+			{
+				if (i == 0)
+				{
+					str1 = line.substr(line.find(':'));
+					str2 = str1.substr(str1.find('"') + 1, str1.rfind('"') - 3);
+					name = str2;
+				}
+				else
+
+				{
+					str1 = line.substr(line.find(':'));
+					str2 = str1.substr(str1.find(' ') + 1, str1.rfind(',') - 2);
+					if (i == 1)
+						hp = std::stoi(str2);
+					if (i == 2)
+						dmg = std::stoi(str2);
+				}
+				i++;
+			}
+		}
+		file.close();
+
+		/*std::cout << name << " " << hp << " " << dmg << std::endl;*/
+	}
+	return Fighter(name, hp, dmg);
+}
+
