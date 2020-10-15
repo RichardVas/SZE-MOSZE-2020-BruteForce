ccbb8c620592a2bbf881b2332cef5ce5c8d394c8
Richard Vas <58149185+RichardVas@users.noreply.github.com>
2020-10-15 23:20:20 +0200

    Implement new leveling feature

--- Fighter.h
+++ Fighter.h
@@ -6,30 +6,37 @@
 
 class Fighter {
 
-	int HP;
-	const int DMG;
+	double MaxHP;
+	double HP;
+	double DMG;
 	const std::string name;
+	int level = 1;
+	int exp = 0;
 
 public:
 
-	Fighter(std::string iname, int ihp, int idmg) : name(iname), HP(ihp), DMG(idmg) {}
+	
+
+	Fighter(std::string iname, double ihp, int idmg) : name(iname), HP(ihp), DMG(idmg),MaxHP(ihp) {}
 	~Fighter() {}
 	void take_dmg(Fighter &enemy);
-	void deal_dmg (Fighter &enemy);
+	void deal_dmg(Fighter &enemy);
 
-	int getHP() const { return HP; }
-	int getDMG() const { return DMG; }
+	double getHP()  { return HP; }
+	double getDMG() { return DMG; }
+	int getLVL() { return level; }
+	int getXP() { return exp; }
+
 	std::string getName() const { return name; }
 
+	void levelUP(Fighter *unit);
 
-	
 
 	friend std::ostream& operator<<(std::ostream& os, const Fighter& fi);
 
 	static Fighter parseUnit(std::string fname);
-		
 
-	
-	
+
+
+
 };
-
