dfc5b7719696482b06d69944efacb6b318ffd78b
Richard Vas <58149185+RichardVas@users.noreply.github.com>
2020-10-15 23:06:10 +0200

    Create new jsonparser class

--- Jsonparser.h
+++ Jsonparser.h
@@ -1,1 +1,14 @@
+#pragma once
+#include"Fighter.h"
+
+class Jsonparser
+{
+public:
+	~Jsonparser() {}
+	Jsonparser() {}
+	static std::string determine_type(std::string str);
+	static std::map<std::string, std::string> getmap(std::string str);
+	static std::map<std::string, std::string> parseJson(std::string fname);
+	static std::map<std::string, std::string> parseJson(std::istream &input);
+};
 
