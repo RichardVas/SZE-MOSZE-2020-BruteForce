37f902a0f09120f722cc5af44ba3098b344549a4
Richard Vas <58149185+RichardVas@users.noreply.github.com>
2020-10-15 23:03:39 +0200

    Implement new determine_type helper method

--- Fighter.cpp
+++ Fighter.cpp
@@ -1,6 +1,17 @@
 #include "Fighter.h"
-
-
+const char* determine_type(std::string str)
+{
+	
+	if (str.find('"') != std::string::npos)
+		return "string";
+	else
+	{
+		if (str.find('.') != std::string::npos)
+			return "double";
+		else
+			return "int";
+	}
+}
 void Fighter::take_dmg(Fighter& enemy) {
 	HP -= enemy.getDMG();
 	if (HP < 0)
@@ -16,51 +27,71 @@ std::ostream& operator<<(std::ostream& os, const Fighter& fi)
 	os << fi.getName() << ": HP: " << fi.getHP() << "," << " DMG: " << fi.getDMG() << std::endl;
 	return os;
 }
-
-
 Fighter Fighter::parseUnit(std::string fname)
 {
 	std::ifstream file;
-
-
 	file.open(fname);
 	if (!file.good()) throw std::runtime_error("File cannot be opened!");
-
 	else {
-		int i = 0;
+	
 		std::string name;
 		int hp;
 		int dmg;
 
 		std::string line;
+		
 		while (std::getline(file, line))
-		{
-
+		{		
 			std::string str1 = "";
 			std::string str2 = "";
-			if (line[0] != '{' && line[0] != '}')
+			std::string str0 = "";
+			std::string str00 = "";
+
+			if(line.find('{')==std::string::npos && line.find('}')==std::string::npos)
+			
 			{
-				if (i == 0)
+				
+				str0 = line.substr(0, line.find(':')-1);
+				
+				str00 = str0.substr(str0.find('"') + 1, (str0.rfind('"') - str0.find('"')) - 1);
+				std::cout << str00 << "\n";
+				
+
+				std::string type = determine_type(line.substr(line.find(':')));
+
+				if (type=="string")
 				{
 					str1 = line.substr(line.find(':'));
-					str2 = str1.substr(str1.find('"') + 1, str1.rfind('"') - 3);
-					name = str2;
+					
+					str2 = str1.substr(str1.find('"')+1, (str1.rfind('"')-str1.find('"'))-1);
+					name = str2;				
+					
+					
 				}
 				else
 
 				{
+					
 					str1 = line.substr(line.find(':'));
-					str2 = str1.substr(str1.find(' ') + 1, str1.rfind(',') - 2);
-					if (i == 1)
+					
+
+					str2 = str1.substr(str1.find(':')+1, str1.rfind(','));
+					if (type == "int") {
 						hp = std::stoi(str2);
-					if (i == 2)
-						dmg = std::stoi(str2);
+
+					}
+					else
+					{
+						if (type == "double")
+						{
+							dmg = std::stoi(str2);
+						}
+					}
 				}
-				i++;
+				
 			}
-		}
+		}		
 		file.close();
-		return Fighter(name, hp, dmg);
+		return Fighter(name, hp, dmg);		
 	}
-	
 }
