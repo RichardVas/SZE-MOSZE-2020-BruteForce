935d73dde4aeb533e7440bbb90505539108ff83e
Richard Vas <58149185+RichardVas@users.noreply.github.com>
2020-10-18 13:54:35 +0200

    Add new call to the duel method

--- Source.cpp
+++ Source.cpp
@@ -8,19 +8,15 @@ int main(int argc, char* argv[])
 	try {
 		Fighter u1(Fighter::parseUnit(argv[1]));
 		Fighter u2(Fighter::parseUnit(argv[2]));
-		u1.duel(&u2);
-		/**
-	 	* Exception handling. An exception thrown when a file related error occurs.
-	 	*/
+
+		Fighter *outcome=&u1.duel(&u2);
+		std::cout << outcome->getName() << " wins. " << "Remaining HP: " << outcome->getHP() << std::endl;
 
 	}
 	catch (const std::exception &e)
 	{
 		std::cout << e.what() << std::endl;
 		return 1;
-		/**
-	 	* Exception handling. Catches and warns about an error occurring.
-	 	*/
 	}
 
 	return 0;
