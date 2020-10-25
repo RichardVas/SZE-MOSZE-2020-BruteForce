af85fb94634c96e69fbb1a9aa7e72a29a0b02275
Richard Vas <58149185+RichardVas@users.noreply.github.com>
2020-10-25 22:30:56 +0100

    Update Jsonparser.h

--- Jsonparser.h
+++ Jsonparser.h
@@ -3,12 +3,19 @@
 
 class Jsonparser
 {
+	
+	enum type { Integer, Double, String };
+	static type determine_type(std::string str);
+
 public:
-	~Jsonparser() {}
-	Jsonparser() {}
-	static std::string determine_type(std::string str);
+
 	static std::map<std::string, std::string> getmap(std::string str);
 	static std::map<std::string, std::string> parseJson(std::string fname);
 	static std::map<std::string, std::string> parseJson(std::istream &input);
-};
 
+	~Jsonparser() {}
+	Jsonparser() {}
+	
+
+
+};
