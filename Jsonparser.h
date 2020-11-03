9b86e80b1a53cf259b43e092ef8d15fbd539a1f1
matevarga96 <57194842+matevarga96@users.noreply.github.com>
2020-11-03 18:56:05 +0100

    Add documentation

--- Jsonparser.h
+++ Jsonparser.h
@@ -3,14 +3,25 @@
 
 class Jsonparser
 {
+	/**
+	*Class for parsing json input files.
+	*/
 	enum type { Integer, Double, String };
-	static type determine_type(std::string str);
+	static type determine_type(std::string str)/**
+	*Method that returns with an enum type of a given string.
+	*/;
 
 public:
 
-	static std::map<std::string, std::string> getmap(std::string str);
-	static std::map<std::string, std::string> parseJson(std::string fname);
-	static std::map<std::string, std::string> parseJson(std::istream &input);
+	static std::map<std::string, std::string> getmap(std::string str)/**
+	*Method that creates the map of a given string, that is in a json format.
+	*/;
+	static std::map<std::string, std::string> parseJson(std::string fname)/**
+	*Method that processes a json file that is given by its name.
+	*/;
+	static std::map<std::string, std::string> parseJson(std::istream &input)/**
+	*Method that processes a json file that is given through an istream.
+	*/;
 
 	~Jsonparser() {}
 	Jsonparser() {}
